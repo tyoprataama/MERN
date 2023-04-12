22d3e7a4424d49b159f18bc1ac608de78e2b3cbf
tyoprataama <tyoprataama004@gmail.com>
2023-04-12 09:34:43 +0700

    set up mongoose

--- app/db/index.js
+++ app/db/index.js
@@ -1,14 +1,10 @@
-// (1) import package mongoose
 const mongoose = require('mongoose');
 
-// (2) kita import konfigurasi terkait MongoDB dari app/config.js
-const { urlDb } = require('../utils/config');
 
-// (3) connect ke MongoDB menggunakan konfigurasi yang telah kita import
+const { urlDb } = require('../config');
+
 mongoose.connect(urlDb);
 
-// (4) simpan koneksi dalam constant db
 const db = mongoose.connection;
 
-// (5) export db supaya bisa digunakan oleh file lain yang membutuhkan
 module.exports = db;
