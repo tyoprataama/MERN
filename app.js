cca2763cd87584b584be9002e6169284b5cababf
tyoprataama <tyoprataama004@gmail.com>
2023-05-21 09:51:41 +0700

    initialize router on app

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(v1, categoriesRouter);
 
 app.get('/', (req, res) => {
   res.status(200).json({
