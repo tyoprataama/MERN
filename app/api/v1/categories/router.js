e2915884edd254fc7703e8eddf41fc46cedff595
tyoprataama <tyoprataama004@gmail.com>
2023-05-21 09:43:43 +0700

    adding post method in router categories

--- app/api/v1/categories/router.js
+++ app/api/v1/categories/router.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express();
+const { create } = require('./controller');
+
+router.post('/categories', create);
 
 router.get('/categories', (req, res) => {
   res.status(200).json({
